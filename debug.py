464ae7bc7dc845490da3caf3c27e1eccc39e23ce
jootiee <purrrtov@ya.ru>
2021-11-20 00:04:56 +0700

    final commit

--- debug.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-
-con = sqlite3.connect("tables/table.db")
-cur = con.cursor()
-
-print([elem for elem in cur.execute("SELECT * FROM sqlite_master WHERE type='table'").fetchall()])
--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from Ui_score_edit import *
-from sql import *
 from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
+from sql import *
+from Ui_score_edit import *
+from Ui_team_edit import *
+import sqlite3
+import os
 
-class Ui_Form(object):
+class App(object):
     def __init__(self, Form):
-        self.score_editor_widget = QtWidgets.QWidget()
-        self.score_editor = Ui_Form_score_edit()
-        self.score_editor.setupUi(self.score_editor_widget)
-        self.tabler = Tabler()
-
         self.form = Form
         self.form.setObjectName("PMT Tabler")
         self.form.setFixedSize(800, 600)
@@ -28,9 +26,6 @@ class Ui_Form(object):
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
         self.tabWidget_tables = QtWidgets.QTabWidget(self.verticalLayoutWidget)
         self.tabWidget_tables.setObjectName("tabWidget_tables")
-        # self.tab_tables_1 = QtWidgets.QWidget()
-        # self.tab_tables_1.setObjectName("tab_tables_1")
-        # self.tabWidget_tables.addTab(self.tab_tables_1, "")
         self.horizontalLayout_2.addWidget(self.tabWidget_tables)
         self.verticalLayout_4.addLayout(self.horizontalLayout_2)
         self.horizontalLayout_4 = QtWidgets.QHBoxLayout()
@@ -46,10 +41,10 @@ class Ui_Form(object):
         self.verticalLayout_3.setObjectName("verticalLayout_3")
         self.pushButton_2 = QtWidgets.QPushButton(self.verticalLayoutWidget)
         self.pushButton_2.setObjectName("pushButton_2")
-        self.verticalLayout_3.addWidget(self.pushButton_2)
         self.pushButton_3 = QtWidgets.QPushButton(self.verticalLayoutWidget)
         self.pushButton_3.setObjectName("pushButton_3")
         self.verticalLayout_3.addWidget(self.pushButton_3)
+        self.verticalLayout_3.addWidget(self.pushButton_2)
         self.verticalLayout_2.addLayout(self.verticalLayout_3)
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setContentsMargins(-1, 50, -1, -1)
@@ -58,119 +53,118 @@ class Ui_Form(object):
         self.pushButton_4 = QtWidgets.QPushButton(self.verticalLayoutWidget)
         self.pushButton_4.setObjectName("pushButton_4")
         self.horizontalLayout.addWidget(self.pushButton_4)
-        self.button_change_view = QtWidgets.QPushButton(
-            self.verticalLayoutWidget)
-        self.button_change_view.setObjectName("button_change_view")
-        self.horizontalLayout.addWidget(self.button_change_view)
         self.verticalLayout_2.addLayout(self.horizontalLayout)
         self.horizontalLayout_4.addLayout(self.verticalLayout_2)
-        self.verticalLayout = QtWidgets.QVBoxLayout()
-        self.verticalLayout.setSpacing(0)
-        self.verticalLayout.setObjectName("verticalLayout")
         self.button_edit_score = QtWidgets.QPushButton(
             self.verticalLayoutWidget)
         self.button_edit_score.setObjectName("pushButton")
-        self.verticalLayout.addWidget(self.button_edit_score)
-        self.horizontalLayout_4.addLayout(self.verticalLayout)
+        self.verticalLayout_3.addWidget(self.button_edit_score)
         self.verticalLayout_4.addLayout(self.horizontalLayout_4)
-    
+
         self.tabs_tables = list()
-
-        self.button_edit_score.clicked.connect(self.show_score_editor)
-
-        self.score_editor.button_save.clicked.connect(lambda: self.score_editor.save(self.update_score))
 
         self.retranslateUi(self.form)
         QtCore.QMetaObject.connectSlotsByName(self.form)
+        self.path_table = QtWidgets.QFileDialog.getOpenFileName(self.form, 'Выбрать картинку', '', 'Таблица (*.db);;Таблица (*.db);;Все файлы (*)')[0]
         self.run()
 
+
     def run(self):
-        self.path_table = QtWidgets.QFileDialog.getOpenFileName(
-            self.form, 'Выбрать картинку', '', 'Таблица (*.db);;Таблица (*.db);;Все файлы (*)')[0]
+        self.score_editor_widget = QtWidgets.QWidget()
+        self.score_editor = Ui_Form_score_edit(self.path_table)
+        self.score_editor.setupUi(self.score_editor_widget)
+
+        self.teams_editor_widget = QtWidgets.QWidget()
+        self.teams_editor = Ui_Form_teams_edit()
+        self.teams_editor.setupUi(self.teams_editor_widget)
+
+
+        self.tabler = Tabler(self.path_table)
+
+
+
+        self.button_edit_score.clicked.connect(self.show_score_editor)
+        self.pushButton_2.clicked.connect(self.show_teams_editor)
+        self.score_editor.button_save.clicked.connect(self.save_score)
+        self.teams_editor.button_edit_player.clicked.connect(self.edit_player)
+        self.teams_editor.button_add_player.clicked.connect(self.add_player)
+        self.teams_editor.button_delete_player.clicked.connect(self.delete_player)
+        self.pushButton_3.clicked.connect(self.new_tour)
+        self.pushButton_4.clicked.connect(self.form.close)
+
+
         self.load_tables()
         self.load_tours()
 
+    def save_score(self):
+        self.score_editor.save()
+        self.update_tables()
+
     def load_tables(self):
         tours = self.tabler.get_tours()
+        self.db = QSqlDatabase.addDatabase('QSQLITE')
+        self.db.setDatabaseName(self.path_table)
+        self.db.open()
+
         for tour in tours:
-            # load of upper tables
-            print(self.path_table)
-            db = QSqlDatabase.addDatabase('QSQLITE')
-            db.setDatabaseName(self.path_table)
-            db.open()
+            self.view1 = QtWidgets.QTableView(self.form)
+            self.view1.verticalHeader().setVisible(False)
+            self.view1.setEditTriggers(
+                QtWidgets.QAbstractItemView.NoEditTriggers)
 
-            view = QtWidgets.QTableView(self.form)
-            view.verticalHeader().setVisible(False)
-            view.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
-
-            model = QSqlTableModel(self.form, db)
+            model = QSqlTableModel(self.form, self.db)
             model.setTable(tour)
             model.select()
 
-            view.setModel(model)
+            self.view1.setModel(model)
 
-            self.tabs_tables.append(view)
-        
-        view = QtWidgets.QTableView(self.form)
+            self.tabs_tables.append(self.view1)
 
-        model = QSqlTableModel(self.form, db)
-        model.setTable("sessions")
-        model.select()
-        
-        view.setModel(model)
-        view.verticalHeader().setVisible(False)
-        view.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
-        view.setColumnHidden(0, True)
-
-        self.verticalLayout.addWidget(view)
-        view.setLayoutDirection(QtCore.Qt.RightToLeft)
-        self.verticalLayout.addWidget(self.button_edit_score)
+    def update_tables(self):
+        tours = self.tabler.get_tours()
+        for tab, tour, index in zip(self.tabs_tables, tours, range(len(tours))):
+            model = QSqlTableModel(self.form, self.db)
+            model.setTable(tour)
+            model.select()
+            tab.setModel(model)
+            self.tabWidget_tables.removeTab(index)
+            self.tabWidget_tables.addTab(
+                tab, "Тур " + str(index + 1))
 
     def load_tours(self):
         tours = self.tabler.get_tours()
+        try:
+            for i in reversed(range(self.tabWidget_tables.count())):
+                self.tabWidget_tables.removeTab(i)
+        except Exception:
+            pass
+        
+
         for index, tour in enumerate(tours):
             index_tour = "".join(list(filter(lambda x: x.isdigit(), tour)))
-            self.tabWidget_tables.addTab(self.tabs_tables[index], "Тур " + index_tour)
+            self.tabWidget_tables.addTab(
+                self.tabs_tables[index], "Тур " + index_tour)
             self.score_editor.combobox_tour.addItem("Тур " + index_tour)
 
-    def update_score(self):
-        pass
-        teams = self.tabler.get_teams()
-        players = self.tabler.get_players()
-
-        for team in teams:
-            for player in players:
-                pass
-                
-
     def show_score_editor(self):
+        tour = self.score_editor.combobox_tour.currentIndex()
+        self.score_editor.load_players(0, tour)
+        self.score_editor.load_players(1, tour)
         self.score_editor_widget.show()
-
-
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Form"))
+        Form.setWindowTitle(_translate("Form", "PMT Tabler"))
         # self.tabWidget_tables.setTabText(self.tabWidget_tables.indexOf(self.tab_tables_1), _translate("Form", "Tab 1"))
         # self.tabWidget_tables.setTabText(self.tabWidget_tables.indexOf(self.tab_tables_2), _translate("Form", "Tab 2"))
         self.pushButton_2.setText(_translate("Form", "Настройка команд"))
         self.pushButton_3.setText(_translate("Form", "Новый тур"))
         self.pushButton_4.setText(_translate("Form", "Выход"))
-        self.button_change_view.setText(_translate("Form", "Сохранить как..."))
+        # self.button_change_view.setText(_translate("Form", "Сохранить как..."))
         self.button_edit_score.setText(_translate("Form", "Настройка счета"))
 
-
     def new_tour(self):
-        tours = self.tabler.get_tours()
-        print(self.tabs_tables)
-        if tours:
-            tours = sorted([int("".join(list(filter(lambda x: x.isdigit(), elem)))) for elem in tours])
-
-            self.tabler.create_tour(tour=tours[-1] + 1)
-        else:
-            self.tabler.create_tour(tour=1)
-        self.load_tours()
-        self.update_tables()
+        self.error_in_progress()
 
     def edit_player(self):
         teams = self.tabler.get_teams()
@@ -229,6 +223,14 @@ class Ui_Form(object):
         msg.setWindowTitle("Ошибка")
         msg.exec_()
 
+    def error_in_progress(self):
+        msg = QtWidgets.QMessageBox()
+        msg.setIcon(QtWidgets.QMessageBox.Critical)
+        msg.setText("Ошибка")
+        msg.setInformativeText('Функция еще в разработке! :)')
+        msg.setWindowTitle("Ошибка")
+        msg.exec_()
+
     def show_teams_editor(self):
         self.teams_editor_widget.show()
 
@@ -237,6 +239,6 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Widget = QtWidgets.QWidget()
-    ui = Ui_Form(Widget)
+    ui = App(Widget)
     Widget.show()
     sys.exit(app.exec_())
